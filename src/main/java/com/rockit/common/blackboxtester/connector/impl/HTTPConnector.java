e27472819b8bf081c80acd365f8370a44d09afaa
Waldemar Müller <Waldemar.Mueller@rockit.consulting>
2018-10-12 14:38:40 +0200

    New advanced HTTPConnector

---
+++ src/main/java/com/rockit/common/blackboxtester/connector/impl/HTTPConnector.java
@@ -0,0 +1,122 @@
+package com.rockit.common.blackboxtester.connector.impl;
+
+import static com.rockit.common.blackboxtester.suite.configuration.ConfigurationHolder.configuration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Base64;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
+
+import com.rockit.common.blackboxtester.connector.ReadConnector;
+import com.rockit.common.blackboxtester.connector.WriteConnector;
+import com.rockit.common.blackboxtester.exceptions.ConnectorException;
+import com.rockit.common.blackboxtester.exceptions.GenericException;
+import com.rockit.common.blackboxtester.suite.configuration.Constants;
+import com.rockit.common.blackboxtester.suite.configuration.PayloadReplacer;
+
+public class HTTPConnector implements ReadConnector, WriteConnector {
+	public static final Logger LOGGER = Logger.getLogger(HTTPConnector.class.getName());
+
+	private StringBuilder resultBuilder;
+	private String name, urlStr;
+	private URL url;
+	private File file;
+
+	public HTTPConnector(String name) {
+		this.name = name;
+		this.urlStr = configuration().getPrefixedString(name, Constants.URL);
+	}
+	@Override
+	public void proceed() {
+
+
+		try {
+
+			
+			URL url = new URL(urlStr);
+			URLConnection urlConnection = url.openConnection();
+			
+			if(  url.getUserInfo() != null && url.getUserInfo().split(":").length==2 ) {
+				LOGGER.debug("Basic authentication usr/pwd >   " + url.getUserInfo());
+				String name = url.getUserInfo().split(":")[0];
+				String password = url.getUserInfo().split(":")[1];
+		        byte[] authEncBytes = Base64.getEncoder().encode( (name + ":" + password).getBytes() );
+		        urlConnection.setRequestProperty("Authorization", "Basic " + new String(authEncBytes));
+			} 
+			
+	        InputStream is = urlConnection.getInputStream();
+	        String result = IOUtils.toString(is);
+
+			setReponse(new StringBuilder().append("<root>").append(parseJSON(result)).append("</root>").toString());
+	         
+		} catch (IOException e) {
+
+			LOGGER.error("can not open connection: " + url, e);
+			throw new ConnectorException(e);
+
+		} catch (JSONException je) {
+			//( [TODO] eigene Exception werfen!!!!!!!!!
+			LOGGER.error("JSON deserialization Exception", je);
+			throw new ConnectorException(je);
+
+		}
+
+	
+		}
+	//( [TODO] eigene Exception werfen!!!!!!!!!
+	public String parseJSON(String result) throws JSONException{
+		try {
+			return XML.toString(new JSONObject(result));
+//			new JSONObject();
+		} catch (JSONException ex) {
+			// e.g. in case JSONArray is valid as well...
+			return XML.toString(new JSONArray(result));
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getId() {
+		return getType() + Constants.FLOW_NAME_SEPARATOR + getName();
+	}
+
+	@Override
+	public String getType() {
+		return Constants.Connectors.HTTPGET.toString();
+	}
+
+	@Override
+	public String getResponse() {
+		return resultBuilder.toString();
+	}
+
+	@Override
+	public void setReponse(String response) {
+		this.resultBuilder = new StringBuilder(response);
+	}
+
+	@Override
+	public void setRequest(final File requestFile) {
+		file = PayloadReplacer.interpolate(requestFile);
+	}
+
+	@Override
+	public void setRequest(final String request) {
+		LOGGER.error("Not supported: ");
+		throw new GenericException("not yet supported");
+
+	}
+
+}
---
+++ src/test/java/com/rockit/common/blackboxtester/suite/structures/HttpConnectorTest.java
@@ -0,0 +1,87 @@
+package com.rockit.common.blackboxtester.suite.structures;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
+import com.rockit.common.blackboxtester.suite.configuration.Constants;
+
+public class HttpConnectorTest {
+	
+	public static final Logger LOGGER = Logger.getLogger(HttpConnectorTest.class.getName());
+	
+	List<String> connectors = ImmutableList.of("HTTP.TESTPOST"
+			);
+	
+    
+	
+	@Test
+	public void testAddStep(){
+		
+		TestBuilder testBuilder = new TestBuilder(HttpConnectorTest.class);
+		TestStepBuilder testStepBuilder = testBuilder.addStep("a001DoHttpTests");
+
+		String recordRootPath = "src/test/resources/";
+		String replayRootPath = "target/replay/";
+		
+		assertTrue(testBuilder.getRecordRootPath(),testBuilder.getRecordRootPath().endsWith(recordRootPath));
+		assertTrue(testBuilder.getReplayRootPath(),testBuilder.getReplayRootPath().endsWith(replayRootPath));
+		
+		String testName = HttpConnectorTest.class.getSimpleName();
+		String recordOutputFolder = "src/test/resources/"+testName + "/" + Constants.OUTPUT_FOLDER;
+		String replayOutputFolder = "target/replay/"+ testName + "/" + Constants.OUTPUT_FOLDER;
+		
+		assertTrue(testBuilder.getRecordOutputFolder() ,testBuilder.getRecordOutputFolder().endsWith(recordOutputFolder));
+		assertTrue(testBuilder.getReplayOutputFolder(),testBuilder.getReplayOutputFolder().endsWith(replayOutputFolder));
+		
+		
+		assertTrue(testBuilder.getInFolderName(), testBuilder.getInFolderName().equals(testName+"/"));;
+		assertTrue(testBuilder.getOutFolderName(), testBuilder.getOutFolderName().equals(testName+"/output/"));;
+		assertTrue(testBuilder.getTestName(), testBuilder.getTestName().equals("HttpConnectorTest"));
+		assertTrue(testStepBuilder.getTestStepName() + "="  + testBuilder.getTestStepName() , testStepBuilder.getTestStepName().equals(  testBuilder.getTestStepName() ));
+		assertTrue(testStepBuilder.getTestStepName(), testStepBuilder.getTestStepName().equals("a001DoHttpTests"));
+
+		
+		
+		for (ConnectorFolder connectorFolder: testStepBuilder.getConnectorFolders() ) {
+			assertTrue(connectors.contains(connectorFolder.getConnectorName()) );
+		}
+		
+		testStepBuilder.execute();
+		
+	}	
+
+//	@Test
+//	public void testAddAssertion() throws Exception {
+//		throw new RuntimeException("not yet implemented");
+//	}
+//
+//	@Test
+//	public void testProceedAssertions() throws Exception {
+//		throw new RuntimeException("not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetMasterRecordFolder() throws Exception {
+//		throw new RuntimeException("not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetTestFolder() throws Exception {
+//		throw new RuntimeException("not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetReplayFolder() throws Exception {
+//		throw new RuntimeException("not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetReplayRecordFolder() throws Exception {
+//		throw new RuntimeException("not yet implemented");
+//	}
+}
