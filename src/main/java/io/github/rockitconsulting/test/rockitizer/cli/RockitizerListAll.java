b6bffb0709b1070c9b2968a5e65c71e81d4e2288
devteamua <dev@sicenter.ua>
2020-02-25 10:59:45 +0100

    cli impl

--- src/main/java/io/github/rockitconsulting/test/rockitizer/cli/RockitizerListAll.java
+++ src/main/java/io/github/rockitconsulting/test/rockitizer/cli/RockitizerListAll.java
@@ -10,7 +10,7 @@ synopsisHeading = "%n",
 descriptionHeading = "%n@|bold,underline Description:|@%n%n",
 parameterListHeading = "%n@|bold,underline Parameters:|@%n",
 optionListHeading = "%n@|bold,underline Options:|@%n",
-header = "Record changes to the repository.",
+header = "(Working) List All",
 description = "Stores the current contents of the index in a new commit " +
     	"along with a log message from the user describing the changes.")
 
