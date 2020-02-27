31e9e3d6456a3dad8b4d8e3a3c974e17f3c53122
devteamua <dev@sicenter.ua>
2020-02-27 16:48:48 +0100

    cli impl

--- src/main/java/io/github/rockitconsulting/test/rockitizer/cli/RockitizerListTC.java
+++ src/main/java/io/github/rockitconsulting/test/rockitizer/cli/RockitizerListTC.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import io.github.rockitconsulting.test.rockitizer.configuration.utils.LogUtils;
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 @CommandLine.Command(name = "list-testcases",
@@ -11,7 +12,7 @@ sortOptions = false, headerHeading = "@|bold,underline Benutzung:|@%n%n",
 synopsisHeading = "%n", descriptionHeading = "%n@|bold,underline Description:|@%n%n",
 parameterListHeading = "%n@|bold,underline Parameters:|@%n",
 optionListHeading = "%n@|bold,underline Options:|@%n",
-header = "(Working) cli list-testcases [<testcaseName>] [<true>]",
+header = "(Working) cli list-testcases [<testcaseName> | <all>] [<-r true>]",
 description = "Stores the current contents of the index in a new commit "
 		+ "along with a log message from the user describing the changes.")
 public class RockitizerListTC implements Runnable {
@@ -19,8 +20,8 @@ public class RockitizerListTC implements Runnable {
 	@Parameters(index = "0", arity = "1", description = "<TestcaseName>")
 	String testcase;
 	
-	@Parameters(index = "1", arity = "0..1", description = ".......")
-    boolean recursive;
+	@Option(names = { "-r", "--recursive" }, arity = "0..1", description = "the archive file")
+	boolean recursive;
 	
 	@Override
 	public void run() {
