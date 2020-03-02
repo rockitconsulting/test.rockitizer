caa3e79698c93afdc650677001392ab8fd3aeacc
devteamua <dev@sicenter.ua>
2020-03-02 11:19:29 +0100

    Update RockitizerDeleteConfig.java

--- src/main/java/io/github/rockitconsulting/test/rockitizer/cli/RockitizerDeleteConfig.java
+++ src/main/java/io/github/rockitconsulting/test/rockitizer/cli/RockitizerDeleteConfig.java
@@ -7,8 +7,7 @@ import java.io.File;
 import picocli.CommandLine;
 import picocli.CommandLine.Parameters;
 
-@CommandLine.Command(name = "delete-env", sortOptions = false, headerHeading = "@|bold,underline Benutzung:|@%n%n", synopsisHeading = "%n", descriptionHeading = "%n@|bold,underline Description:|@%n%n", parameterListHeading = "%n@|bold,underline Parameters:|@%n", optionListHeading = "%n@|bold,underline Options:|@%n", header = "cli delete-env [<env>]", description = "Stores the current contents of the index in a new commit "
-		+ "along with a log message from the user describing the changes.")
+@CommandLine.Command(name = "delete-env", sortOptions = false, headerHeading = "@|bold,underline Benutzung:|@%n%n", synopsisHeading = "%n", descriptionHeading = "%n@|bold,underline Description:|@%n%n", parameterListHeading = "%n@|bold,underline Parameters:|@%n", optionListHeading = "%n@|bold,underline Options:|@%n", header = "cli delete-env [<env>]", description = "Delete environment")
 public class RockitizerDeleteConfig implements Runnable {
 
 	@Parameters(index = "0", arity = "0..1", description = ": env = dev => deletion of resources-dev.yaml")
