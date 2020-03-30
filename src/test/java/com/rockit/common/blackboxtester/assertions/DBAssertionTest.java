75b000c79d40bbd7f55c53192424cfd87f8ce1c7
devteamua <dev@sicenter.ua>
2020-03-30 09:08:36 +0200

    Update DBAssertionTest.java
    DBAssertionTest.java with annotations.

--- src/test/java/com/rockit/common/blackboxtester/assertions/DBAssertionTest.java
+++ src/test/java/com/rockit/common/blackboxtester/assertions/DBAssertionTest.java
@@ -23,7 +23,7 @@ public class DBAssertionTest {
 	 * DB assertion with 2 empty parameter values: sqlQuery, mustContainTokens.  
 	 * 
 	 */
-
+	@Test
 	public void testToStringTwoNullParms() throws IOException {
 		DBAssertion TestDBAssertion = new DBAssertion(null, null);
 		assertNotNull(TestDBAssertion.toString());
@@ -33,7 +33,7 @@ public class DBAssertionTest {
 	 * DB assertion with 3 sample parameter values: datasourceId, sqlQuery, mustContainTokens. 
 	 */
 
-	
+	@Test
 	public void testWithThreeParms() throws IOException {
 		DBAssertion TestDBAssertion = new DBAssertion("defaultDB", "SELECT * FROM ROCKIT.BOOKSERVICE;", ImmutableList.of("createdAt", "id"));
 		assertNotNull(TestDBAssertion.toString());
@@ -43,9 +43,9 @@ public class DBAssertionTest {
 	 * DB assertion with 3 empty parameter values: datasourceId, sqlQuery, mustContainTokens. 
 	 */
 	
-	
+	@Test
 	public void testWithThreeNullParms() throws IOException {
-		DBAssertion TestDBAssertion = new DBAssertion(null, null);
+		DBAssertion TestDBAssertion = new DBAssertion(null, null, null);
 		assertNotNull(TestDBAssertion.toString());
 	}		
 }
