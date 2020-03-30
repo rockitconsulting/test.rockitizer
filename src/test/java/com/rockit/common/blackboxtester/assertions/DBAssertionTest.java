986a1c89e37a8780e33c42363ab3f57583b90ac8
devteamua <dev@sicenter.ua>
2020-03-30 08:40:32 +0200

    Created DBAssertionTest.java
    DBAssertionTest.java with testing of .toString() created.

---
+++ src/test/java/com/rockit/common/blackboxtester/assertions/DBAssertionTest.java
@@ -0,0 +1,51 @@
+package com.rockit.common.blackboxtester.assertions;
+
+import static org.junit.Assert.*;
+import java.io.IOException;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
+
+public class DBAssertionTest {
+	public static Logger LOGGER = Logger.getLogger(DBAssertionTest.class.getName());
+	DBAssertion DBAssertion;
+	/**
+	 * DB assertion with 2 sample parameter values: sqlQuery, mustContainTokens.  
+	 * 
+	 */
+	@Test
+	public void testToStringTwoParms() throws IOException {
+		DBAssertion TestDBAssertion = new DBAssertion("SELECT * FROM ROCKIT.BOOKSERVICE;", ImmutableList.of("createdAt", "id"));
+		assertNotNull(TestDBAssertion.toString());
+	}
+	/**
+	 * DB assertion with 2 empty parameter values: sqlQuery, mustContainTokens.  
+	 * 
+	 */
+
+	public void testToStringTwoNullParms() throws IOException {
+		DBAssertion TestDBAssertion = new DBAssertion(null, null);
+		assertNotNull(TestDBAssertion.toString());
+	}	
+	
+	/**
+	 * DB assertion with 3 sample parameter values: datasourceId, sqlQuery, mustContainTokens. 
+	 */
+
+	
+	public void testWithThreeParms() throws IOException {
+		DBAssertion TestDBAssertion = new DBAssertion("defaultDB", "SELECT * FROM ROCKIT.BOOKSERVICE;", ImmutableList.of("createdAt", "id"));
+		assertNotNull(TestDBAssertion.toString());
+	}
+
+	/**
+	 * DB assertion with 3 empty parameter values: datasourceId, sqlQuery, mustContainTokens. 
+	 */
+	
+	
+	public void testWithThreeNullParms() throws IOException {
+		DBAssertion TestDBAssertion = new DBAssertion(null, null);
+		assertNotNull(TestDBAssertion.toString());
+	}		
+}
