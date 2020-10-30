5d74913a67e4bb6f0b01f8c15994a5d72f8ccb5f
devteamua <dev@sicenter.ua>
2020-10-30 12:59:02 +0100

    env implementation

--- src/main/java/io/github/rockitconsulting/test/rockitizer/configuration/model/EnvironmentsHolder.java
+++ src/main/java/io/github/rockitconsulting/test/rockitizer/configuration/model/EnvironmentsHolder.java
@@ -1,52 +1,47 @@
 package io.github.rockitconsulting.test.rockitizer.configuration.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-/**
-*  Test.Rockitizer - API regression testing framework 
-*   Copyright (C) 2020  rockit.consulting GmbH
-*
-*   This program is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see http://www.gnu.org/licenses/.
-*
-*/
-
-public class EnvironmentsHolder  {
-	
-	private List<Environment> envs 	  =  new ArrayList<>();	
-	
-	
-
-	public EnvironmentsHolder() {}
+import java.util.Map;
 
 
+public class EnvironmentsHolder {
 
+	private List<Environment> envs = new ArrayList<>();
+
+	public EnvironmentsHolder() {
+	}
 
 	public List<Environment> getEnvs() {
 		return envs;
 	}
 
-
-
-
 	public void setEnvs(List<Environment> envs) {
 		this.envs = envs;
 	}
+	
+	
+	/**
+	 * First environment is always used as default. The other environments in list override the  values of the first (default) environment if specified
+	 * 
+	 * @param pEnvName target Environment 
+	 * @return
+	 */
+	public Map<String,String> getEnvByName(String pEnvName) {
+		Map<String,String> props = new HashMap<>();
+		this.getEnvs().forEach( env -> {
+			if(props.isEmpty() || env.getName().equalsIgnoreCase(pEnvName)) {
+				props.putAll( env.getProps() );
+			}			
+		});
+		return props;
+	}
+	
+	
+	public boolean checkExistsEnvByName(String pEnvName) {
+		return   (getEnvs().stream().filter(e -> e.getName().equalsIgnoreCase(pEnvName)).findFirst().orElse(null)) !=null?true:false ;
+		
+	}
 
-	
-	
-	
-	
-	
 }
--- src/test/java/io/github/rockitconsulting/test/rockitizer/configuration/EnvironmentsHolderAccessorTest.java
+++ src/test/java/io/github/rockitconsulting/test/rockitizer/configuration/EnvironmentsHolderAccessorTest.java
@@ -5,7 +5,9 @@ import io.github.rockitconsulting.test.rockitizer.configuration.model.Environmen
 import io.github.rockitconsulting.test.rockitizer.configuration.utils.ConfigUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
@@ -14,25 +16,6 @@ import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
 
-/**
-*  Test.Rockitizer - API regression testing framework 
-*   Copyright (C) 2020  rockit.consulting GmbH
-*
-*   This program is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see http://www.gnu.org/licenses/.
-*
-*/
-
 public class EnvironmentsHolderAccessorTest {
 
 	public static Logger log = Logger.getLogger(EnvironmentsHolderAccessorTest.class.getName());
@@ -40,10 +23,8 @@ public class EnvironmentsHolderAccessorTest {
 	final String rootPath = ConfigUtils.getAbsolutePathToResources();
 	final String relPath = "full.demo.project.cli.tests/src/test/resources/";
 	String rootPathTestSrc = rootPath + relPath;
-    
-	EnvironmentsHolderAccessor ehCLI = new EnvironmentsHolderAccessor(); 
-	
 
+	EnvironmentsHolderAccessor ehCLI = new EnvironmentsHolderAccessor();
 
 	@Before
 	public void before() {
@@ -53,46 +34,74 @@ public class EnvironmentsHolderAccessorTest {
 		log.info(ehCLI.contextAsString());
 
 	}
-	
-	
+
 	@Test
 	public void generateResources() throws IOException {
 		EnvironmentsHolder eh = new EnvironmentsHolder();
 
-		Environment env1 =  new Environment();
+		Environment env1 = new Environment();
 		env1.setName("myenv1");
-		env1.setProps(ImmutableMap.of("k1","v1","k2","v2","k3","v3"));
+		env1.setProps(ImmutableMap.of("k1", "v1", "k2", "v2", "k3", "v3"));
 		eh.getEnvs().add(env1);
-		Environment env2 =  new Environment();
+		Environment env2 = new Environment();
 		env2.setName("myenv2");
-		env2.setProps(ImmutableMap.of("k1","v1","k2","v2","k3","v3"));
+		env2.setProps(ImmutableMap.of("k1", "v1", "k2", "v2", "k3", "v3"));
 		eh.getEnvs().add(env2);
 		Environment env3 = new Environment();
 		env3.setName("myenv3");
-		env3.setProps(ImmutableMap.of("k3","zzzzzzz3"));
+		env3.setProps(ImmutableMap.of("k3", "zzzzzzz3"));
 		eh.getEnvs().add(env3);
-		
+
 		ehCLI.setEnvsFileName("envs-generated.yaml");
 		ehCLI.envsHolderToYaml(eh);
 
-		
-		
 		Assert.assertNotNull(ehCLI);
 		Assert.assertTrue(new File(ehCLI.getFullPath() + ehCLI.getEnvsFileName()).exists());
-		
-		
-		
 
 	}
 
-	
 	@Test
 	public void readResources() throws IOException {
-		
+
 		this.generateResources();
 		EnvironmentsHolder envsHolderFromYaml = ehCLI.envsHolderFromYaml();
-		Assert.assertTrue(envsHolderFromYaml.getEnvs().size()==3);
-		
-	
+		Assert.assertTrue(envsHolderFromYaml.getEnvs().size() == 3);
+
 	}
+
+	@Test(expected = FileNotFoundException.class)
+	public void noEnvExists() throws IOException {
+		ehCLI.setEnvsFileName("notexists.yaml");
+		EnvironmentsHolder envsHolderFromYaml = ehCLI.envsHolderFromYaml();
+		Assert.assertTrue(envsHolderFromYaml.getEnvs().size() == 3);
+
+	}
+
+	@Test
+	public void getConfigByEnv() throws IOException {
+		this.generateResources();
+		EnvironmentsHolder envsHolderFromYaml = ehCLI.envsHolderFromYaml();
+		Assert.assertTrue(envsHolderFromYaml.getEnvs().size() == 3);
+		Assert.assertTrue(envsHolderFromYaml.getEnvs().get(0).getName().equalsIgnoreCase("myenv1"));
+
+		Map<String, String> props = envsHolderFromYaml.getEnvByName("myEnv3");
+
+		Assert.assertTrue(props.size() == 3);
+		Assert.assertTrue(props.containsKey("k1"));
+		Assert.assertTrue(props.containsKey("k2"));
+		Assert.assertTrue(props.containsKey("k3"));
+		Assert.assertTrue(props.get("k1").equalsIgnoreCase("v1"));
+		Assert.assertTrue(props.get("k2").equalsIgnoreCase("v2"));
+		Assert.assertTrue(props.get("k3").equalsIgnoreCase("zzzzzzz3"));
+
+	}
+
+	@Test
+	public void checkEnvByNameExists() throws IOException {
+		this.generateResources();
+		EnvironmentsHolder envsHolderFromYaml = ehCLI.envsHolderFromYaml();
+		Assert.assertTrue(envsHolderFromYaml.checkExistsEnvByName("myenv1"));
+		Assert.assertFalse(envsHolderFromYaml.checkExistsEnvByName("Iamnotexistingenv"));
+	}
+
 }
